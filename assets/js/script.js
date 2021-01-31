1d3cada9bf71abbe7cd4f061e375286109c4df46
Justin Welch <justin.welch075@gmail.com>
2021-01-31 17:12:02 -0600

    added functions for displaying the forecast and appending them to the html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,7 +128,120 @@ function currentWeather(city)
             })
 }
 
+// create forecast items
+function forecast(cityId) 
+    {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?id="
+        + cityId + apiKey + "&units=imperial")
+        .then(function(response)
+            {
+                return response.json();
+            })
+        .then(function(response)
+            {
+                // console.log(response)
 
+                for (i=0; i<5; i++) 
+                    {
+
+                        var date = new Date(response.list[((i+1)*8)-1].dt*1000).toLocaleDateString();
+                        var iconcode = response.list[((i+1)*8)-1].weather[0].icon;
+                        var iconUrl = "https://openweathermap.org/img/wn/"+iconcode+".png";
+                        var tempF = response.list[((i+1)*8)-1].main.temp;
+                        var humidity = response.list[((i+1)*8)-1].main.humidity;
+
+                        $("#forecastDate"+i).html(date);
+                        $("#forecastImg"+i).html("<img src="+iconUrl+">");
+                        $("#forecastTemp"+i).html(tempF+"&#8457");
+                        $("#forecastHumidity"+i).html(humidity+"%");
+                    }
+
+            });
+    }
+
+// add searched cities to list
+function addToList(newCity)
+    {
+
+        // when a city is searched
+        if (searchCity.val() !== "") 
+            {
+                newCity = searchCity.val().trim();
+        
+                //if the city is already added leave function
+                for (i=0; i<cityArray.length; i++) 
+                    {
+                        if (newCity.toUpperCase() === cityArray[i]) 
+                            {
+                                return;
+                            }
+                    }
+
+                console.log("New city added: " + newCity)
+                cityArray.push(newCity.toUpperCase());
+                localStorage.setItem("storedCities",JSON.stringify(cityArray));
+
+                var listEl= $("<a>"+newCity.toUpperCase()+"</a>");
+                $(listEl).attr("class","list-group-item list-group-item-action");
+                $(listEl).attr("data-value",newCity.toUpperCase());
+                $(".list-group").append(listEl);
+                $("#searchCity").val("")
+            }
+        // when there is no search / when page loads
+        else 
+            {
+                var listEl= $("<a>"+newCity.toUpperCase()+"</a>");
+                $(listEl).attr("class","list-group-item list-group-item-action");
+                $(listEl).attr("data-value",newCity.toUpperCase());
+                $(".list-group").append(listEl);
+                $("#searchCity").val("")
+            }
+    }
+
+// when city from the list is clicked
+function changeCity(event)
+    {
+        var liEl=event.target;
+        if (event.target.matches("a"))
+            {
+                city=liEl.textContent.trim();
+                currentWeather(city);
+            }
+    }
+
+//loads cityArrray from storage, lists each item from the array, displays weather for first item
+function loadlastCity()
+    {
+
+        var cityArray = JSON.parse(localStorage.getItem("storedCities"));
+
+        if(cityArray!==null)
+            {
+
+                city=cityArray[0];
+                currentWeather(city);
+
+                for(i=0; i<cityArray.length;i++)
+                    {
+                        // console.log(cityArray[i])
+                        addToList(cityArray[i]);
+                    } 
+            }
+        else 
+            {
+                // console.log("cityArray is empty")
+                cityArray = []
+            }
+    }
+
+//Clear the search history from the page, clear localStorage, reload page
+function clearHistory(event)
+    {
+        event.preventDefault();
+        cityArray=[];
+        localStorage.clear();
+        document.location.reload();
+    }
 
 $("#searchBtn").on("click",displayWeather);
 $("#searchBtn").on("click",addToList);
